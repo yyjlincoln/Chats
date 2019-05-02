50f431f5323998a4a479ae2bc5e14455db866921
Yijun Yan <Yijun.Yan@DET>
2019-05-02 11:11:34 +1000

    Update

--- chat-serv.py
+++ chat-serv.py
@@ -1,17 +1,71 @@
-import socket
-import json
+# All Exceptions
+class HostingException(BaseException):
+    def __init__(self,r,errcode):
+        return
 
-s=socket.socket()
-s.bind(('',8088))
-s.listen(10)
-while True:
-    try:
-        sx,addr=s.accept()
-        d=sx.recv(2048).decode()
-        print(d)
-        e=json.loads(d)
-        print(e)
-        sx.send(json.dumps({'success':True}).encode())
-        sx.close()
-    except:
-        continue
+# Launch Listening Thread
+class WebHostWaiting(threading.Thread):
+    def __init__(self,soc):
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        self.setName('WebHosting Accepting - MAIN')
+        self.soc=soc
+    
+    def run(self):
+        try:
+            soc=self.soc
+            if not soc:
+                raise HostingException('No socket connection is provided',-1)
+            while True:
+                try:
+                    sx,addr=soc.accept()
+                    t=WebHostAccepted(sx,addr)
+                    t.start()
+                except:
+                    pass
+        except:
+            pass
+
+class WebHostAccepted(threading.Thread):
+    def __init__(self,sx,addr):
+        threading.Thread.__init__(self)
+        self.sx=sx
+        self.addr=addr
+        self.setDaemon(True)
+        self.setName('WebHosting Accepted - '+addr[0]+':'+str(addr[1]))
+
+    def run(self):
+        try:
+            sx=self.sx
+            addr=self.addr
+            d=sx.recv(204800000).decode()
+
+
+            #[TODO]
+            
+
+            sx.shutdown(socket.SHUT_RDWR)
+            sx.close()
+        except Exception as e:
+            print(e)
+            pass
+
+
+
+# import socket
+# import json
+
+# s=socket.socket()
+# s.bind(('',8088))
+# s.listen(10)
+# while True:
+#     try:
+#         sx,addr=s.accept()
+#         d=sx.recv(2048).decode()
+#         print(d)
+#         e=json.loads(d)
+#         print(e)
+#         sx.send(json.dumps({'success':True}).encode())
+#         sx.close()
+#     except:
+#         continue
